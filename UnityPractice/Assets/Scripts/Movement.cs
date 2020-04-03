9f19c0607a5a9590374be3df4f6df8ab9f2aeb06
Ian Joshi <ianjoshi@yahoo.com>
2020-04-03 11:22:57 -0500

    Pushed Unity Folder

---
+++ UnityPractice/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject runner;
+    public Transform pivot;
+    public float speed = 1;
+    public GameObject[] bees; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        runner.transform.RotateAround(pivot.position, Vector3.up, Time.deltaTime * speed);
+        for (int i = 0; i < bees.Length; i++)
+        {
+            bees[i].transform.RotateAround(runner.transform.position, Vector3.up, speed);
+            bees[i].transform.RotateAround(runner.transform.position, Vector3.right, speed);
+        }
+      
+    }
+}
+
+
---
+++ UnityPractice/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    // Used Fixed Update because we use physics
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        //force in x direction
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+       
+        
+     
+    }
+}
